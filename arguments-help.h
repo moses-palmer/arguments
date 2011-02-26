0967fd849f95e6c387e4ad9048d3c19fd32158b7
Moses Palmér <mosespalmer@gmail.com>
2011-02-26 23:30:48 +0100

    Added files

---
+++ arguments-help.h
@@ -0,0 +1,251 @@
+#include <ctype.h>
+#include <wchar.h>
+
+/**
+ * Returns the width of the terminal.
+ *
+ * @return the width of the terminal, or (unsigned int)-1 if it cannot be
+ *   determined
+ */
+static unsigned int
+arguments_terminal_width(void)
+{
+    int result = atoi(getenv("COLUMNS"));
+
+    if (!result) {
+        result = (unsigned int)-1;
+    }
+
+    return result;
+}
+
+/**
+ * Returns the number of characters needed for the argument names header.
+ *
+ * @return the number of character needed for the argument names header
+ */
+static unsigned int
+argument_header_width(void)
+{
+    unsigned int current;
+    unsigned int result;
+
+    result = 0;
+
+    /* Calculate how many characters we need for the argument names */
+#undef _A
+#define _A(type, name, short, value_count, is_required, help, \
+        set_default, read, release) \
+    current = 2 + strlen(#name); \
+    if (short) { \
+        current += 2 + strlen(short); \
+    } \
+    if (current > result) { \
+        result = current; \
+    }
+#include "arguments.def"
+
+    return result;
+}
+
+#define isend(c) \
+    (((c) == 0) || ((c) == '\n'))
+
+/**
+ * Determines the number of characters starting at s to print and the offset to
+ * the next line.
+ *
+ * @param s
+ *     The string to investigate. *s has to point to the first character of a
+ *     line.
+ * @param length
+ *     The number of characters to print is written to this variable.
+ * @param max_width
+ *     The maximum number of characters to print.
+ * @return the offset to the beginning of the next line
+ *
+ */
+static unsigned int
+arguments_get_line(const char *s, unsigned int *length,
+    unsigned int max_length)
+{
+    mbstate_t mbs;
+    int was_space, seen_space;
+    unsigned int word_count, end, l, i, result;
+
+    /* Initialise the multibyte state */
+    memset(&mbs, 0, sizeof(mbs));
+    if (!mbsinit(&mbs)) {
+        char outbuf[8];
+        const wchar_t empty[] = L"";
+        const wchar_t *srcp = empty;
+        wcsrtombs(outbuf, &srcp, sizeof(outbuf), &mbs);
+    }
+
+    *length = 0;
+    was_space = 0;
+    seen_space = 0;
+    word_count = 0;
+    end = strlen(s);
+    l = 0;
+    i = 0;
+    result = 0;
+
+    for (i = 0; (i < end) && (l < max_length);) {
+        int is_space;
+        size_t di;
+
+        /* Break immediately if we reach newline */
+        if (s[i] == '\n') {
+            *length = l;
+            result = i + 1;
+            break;
+        }
+
+        if (!seen_space) {
+            /* If we have not yet encountered any space, we have to update the
+               length of the line, since the word will then have to be squeezed
+               in */
+            *length = l;
+            result = i;
+        }
+
+        /* We are processing a character; increase the length */
+        l++;
+
+        is_space = isspace(s[i]);
+        seen_space |= is_space;
+        if (is_space && !was_space) {
+            /* If the current character is space following non-space, the
+               previous character was the last character of a word and we update
+               the length */
+            *length = l - 1;
+        }
+        else if (!is_space && was_space) {
+            /* If this is a non-space character following a space character,
+               this is the first character of a word and we update the offset of
+               the next line */
+            result = i;
+        }
+        was_space = is_space;
+
+        /* Calculate how many bytes we need to increment our position for the
+           current character */
+        di = mbrlen(s + i, end - i, &mbs);
+        switch (di) {
+        case 0:
+            /* This should really not happen */
+            break;
+
+        case (size_t)-1:
+            /* Invalid character sequence; skip to the next byte */
+            i++;
+            break;
+
+        case (size_t)-2:
+            /* Possibly incomplete multi byte sequence; skip to the next
+               byte */
+            i++;
+            break;
+
+        default:
+            i += di;
+        }
+
+        /* If this is the last character of the string, we update length and
+           result to make sure that all characters are included and the caller
+           knows that the string has been terminated */
+        if (i == end) {
+            *length = l;
+            result = i;
+        }
+    }
+
+    /* Skip any trailing space so that the next offset will be the start of a
+       word or the end of the string */
+    while ((result < end) && (s[result] == ' ')) result++;
+
+    return result;
+}
+
+/**
+ * Prints the help for a single argument.
+ *
+ * @param header
+ *     The argument header. This should be "--long-name" or "--long-name, -s".
+ * @param help
+ *     The help string.
+ * @param header_width
+ *     The width of the header column.
+ * @param terminal_width
+ *     The width of the terminal.
+ */
+static void
+arguments_print_help_string(const char *header, const char *help,
+    unsigned int header_width, unsigned int terminal_width)
+{
+    const char *c;
+
+    /* Print the header, which is the argument names left aligned and padded up
+       to names_width characters */
+    printf("\n%-*s ", header_width, header);
+
+    c = help;
+    while (*c) {
+        unsigned int n, length;
+
+        /* Get the length of the current line, and the offset of the start of
+           the next line */
+        n = arguments_get_line(c, &length, terminal_width - header_width);
+
+        /* Print at most length charaters from the current offset in the help
+           string */
+        printf("%-.*s\n", length, c);
+        c += n;
+
+        /* If we have not reached the end of the help string, print a new empty
+           header column */
+        if (*c) {
+            int i;
+
+            for (i = 0; i <= header_width; i++) {
+                putc(' ', stdout);
+            }
+        }
+    }
+}
+
+/**
+ * Prints the help for all commands.
+ */
+static void
+arguments_print_help(void)
+{
+    unsigned int terminal_width;
+    int header_width;
+    char header[128];
+
+    /* Determine the width of the termihnal */
+    terminal_width = arguments_terminal_width();
+
+    /* Determine how many columns we need for the argument names */
+    header_width = argument_header_width();
+
+#ifdef ARGUMENTS_HELP
+    /* Print the help header */
+    printf("%s\n", ARGUMENTS_HELP);
+#endif
+
+    /* Print the argument help strings */
+#undef _A
+#define _A(type, name, short, value_count, is_required, help, \
+        set_default, read, release) \
+    if (short) { \
+        snprintf(header, sizeof(header), "--%s, %s", #name, short); \
+    } \
+    else { \
+        snprintf(header, sizeof(header), "--%s", #name); \
+    } \
+    arguments_print_help_string(header, help, header_width, terminal_width);
+#include "arguments.def"
+}
---
+++ arguments.h
@@ -0,0 +1,533 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <memory.h>
+
+/**
+ * This is the macro used to define command line parameters. Create the file
+ * arguments.def and populate it with invocations of this macro.
+ *
+ * The order of the arguments found in arguments.def needs to reflect the
+ * parameters to to the start function in start.c if ARGUMENTS_AUTOMATIC is
+ * defined.
+ *
+ * @param type
+ *     The type of the variable that stores the parsed value of this argument.
+ *
+ * @param name
+ *     The name of the variable, and also the long argument name. The long
+ *     argument name is constructed by prepending two dashes to the name and
+ *     then replacing all underscores with dashes.
+ *
+ * @param short
+ *     The short command line argument name. This may be NULL.
+ *
+ * @param value_count
+ *     The number of parameters following the named parameter that are required.
+ *
+ * @param is_required
+ *     Whether this argument is required on the command line. The value for
+ *     is_required is evaluated once all arguments have been read, so it may
+ *     depend on arguments passed on the command line.
+ *
+ * @param help
+ *     The help text for the argument. The first occurrence of "%s" in this
+ *     message will be replaced by the default value.
+ *
+ * @param set_default
+ *     A list of statements executed when the argument has not been passed on
+ *     the command line. The following variables are available to the code:
+ *       - type *target: a pointer to the variable to receive the value.
+ *     If there is not suitable default value for the argument, pass
+ *     ARGUMENTS_NO_DEFAULT.
+ *
+ * @param read
+ *     A list of statements used to actually set the value of the variable. The
+ *     following variables are available to the code:
+ *       - type *target: a pointer to the variable to receive the value.
+ *       - const char *value: the actual value passed.
+ *       - int is_valid: whether the value passed was valid; set this to 0 if
+ *         the argument variable could not be initialised.
+ *
+ * @param release
+ *     A list of statements used to release any resources used by the variable;
+ *     this may be closing files, or freeing allocated memory. The following
+ *     variables are available to the code:
+ *       - type *target: a pointer to the variable that received the value.
+ */
+#define _A(type, name, short, value_count, is_required, help, \
+    set_default, read, release)
+
+/**
+ * Pass this value as short if the argument does not have a short name.
+ */
+#define ARGUMENT_NO_SHORT_OPTION NULL
+
+/**
+ * Pass this value as is_required to the _A macro if the argument is required.
+ */
+#define ARGUMENT_IS_REQUIRED 1
+
+/**
+ * Pass this value as is_required to the _A macro if the argument is not
+ * required.
+ */
+#define ARGUMENT_IS_OPTIONAL 0
+
+/**
+ * Checks that a command line argument has been passed.
+ */
+#define ARGUMENT_IS_PRESENT(name) \
+    arguments.name.present
+
+/**
+ * Reads the value of an argument
+ */
+#define ARGUMENT_VALUE(name) \
+    arguments.name.value
+
+/**
+ * We include arguments.def to define anything in the ARGUMENTS_HELPERS section
+ * and the types of the arguments.
+ */
+#undef _A
+#define _A(type, name, short, value_count, is_required, help, \
+        set_default, read, release) \
+    typedef type name##_t;
+#include "arguments.def"
+
+/**
+ * The return code when an invalid command line parameter is encountered.
+ */
+ #ifndef ARGUMENTS_PARAMETER_INVALID
+    #define ARGUMENTS_PARAMETER_INVALID 110
+#endif
+
+/**
+ * The return code when a required command line parameter is missing.
+ */
+#ifndef ARGUMENTS_PARAMETER_MISSING
+    #define ARGUMENTS_PARAMETER_MISSING 120
+#endif
+
+/**
+ * Whether to use this file in automatic mode.
+ */
+#ifndef ARGUMENTS_AUTOMATIC
+    #define ARGUMENTS_AUTOMATIC
+#endif
+
+/**
+ * Whether to always print help if the --help argument is passed.
+ *
+ * If ARGUMENTS_HELP is also defined, it is used as a string parameter to
+ * printf.
+ */
+#ifndef ARGUMENTS_PRINT_HELP
+    #define ARGUMENTS_PRINT_HELP
+#endif
+
+/**
+ * Whether to print an error message when a missing argument is encountered.
+ */
+#ifndef ARGUMENTS_PRINT_MISSING
+    #define ARGUMENTS_PRINT_MISSING
+#endif
+
+
+#ifdef ARGUMENTS_AUTOMATIC
+
+/**
+ * This function is called before the presence of all required arguments has
+ * been verified, but before any arguments are converted to actual values.
+ *
+ * Use this function to initialise any external resources that are required by
+ * the argument readers.
+ *
+ * If this function does not return 0, the program is terminated with the exit
+ * code reurned by this function.
+ *
+ * If you do not need any setup, define ARGUMENTS_NO_SETUP.
+ */
+static int
+setup(int argc, char *argv[])
+#ifdef ARGUMENTS_NO_SETUP
+{}
+#else
+;
+#endif
+
+/**
+ * This is the function called by main.c once the arguments have been parsed if
+ * ARGUMENTS_AUTOMATIC is defined.
+ *
+ * Its function signature matches that of main with all arguments found in
+ * arguments.def added to the argument list as well.
+ */
+#undef _A
+#define _A(type, name, short, value_count, is_required, help, \
+        set_default, read, release) \
+    , name##_t name
+static int
+run(int argc, char *argv[]
+    #include "arguments.def"
+    );
+
+/**
+ * This function is called before the application terminates if setup has been
+ * called.
+ *
+ * If you do not need any teardown, define ARGUMENTS_NO_TEARDOWN.
+ */
+static void
+teardown(void)
+#ifdef ARGUMENTS_NO_TEARDOWN
+{}
+#else
+;
+#endif
+
+#endif
+
+
+/**
+ * Return values used by arguments_validate and arguments_parse.
+ */
+enum {
+    /**
+     * The operation completed successfully.
+     */
+    AC_OK,
+
+    /**
+     * An invalid value was passed on the command line, or a required argument
+     * was missing.
+     */
+    AC_ERROR,
+
+    /**
+     * The command line argument --help was found, and the help message was
+     * printed.
+     *
+     * Unless ARGUMENTS_PRINT_HELP is defined, this value will not be returned.
+     */
+    AC_HELP
+};
+
+
+/**
+ * Compares a long argument to a name string.
+ *
+ * The first two characters of longarg must be "--".
+ *
+ * @param longarg
+ *     The long argument.
+ * @param name
+ *     The name of the argument.
+ * @return 0 if longarg is the long argument for name, non-zero otherwise
+ */
+static int
+arguments_cmp(const char *longarg, const char *name)
+{
+    /* Skip the first two characters */
+    if (*(longarg++) != '-') return -1;
+    if (*(longarg++) != '-') return -1;
+
+    /* Compare the rest of the characters one by one, converting '_' in name to
+       '-' */
+    while (*longarg && *name) {
+        if (*longarg != (*name == '_' ? '-' : *name)) {
+            break;
+        }
+        longarg++;
+        name++;
+    }
+
+    return *longarg - (*name == '_') ? '-' : *name;
+}
+
+
+/**
+ * The struct that contains all argument values.
+ *
+ * For every argument, a single struct is created containing
+ *   * present: whether this argument was present on the command line
+ *   * initialized: whether this argument has been initialised
+ *   * value_strings: the value passed for the argument on the command line, if
+ *     any
+ *   * value_strings_length: the number of string values in value_strings
+ *   * value: its value after it has been parsed
+ */
+static struct {
+#undef _A
+#define _A(type, name, short, value_count, is_required, help, \
+        set_default, read, release) \
+    struct { \
+        int present; \
+        int initialized; \
+        char **value_strings; \
+        unsigned int value_strings_length; \
+        name##_t value; \
+    } name;
+#include "arguments.def"
+} arguments;
+
+
+/**
+ * Initialises the arguments struct by zeroing it.
+ *
+ * Unless ARGUMENTS_AUTOMATIC is defined, this should be called before calling
+ * arguments_parse.
+ */
+static void
+arguments_initialize(void)
+{
+    memset(&arguments, 0, sizeof(arguments));
+}
+
+
+#if defined(ARGUMENTS_PRINT_HELP) || defined(ARGUMENTS_AUTOMATIC)
+    #include "arguments-help.h"
+#endif
+
+
+/**
+ * Releases all arguments that have been initialised.
+ *
+ * If arguments_set has been called, this function must also be called, even if
+ * arguments_set returned an error.
+ */
+static void
+arguments_release(void)
+{
+#undef _A
+#define _A(type, name, short, value_count, is_required, help, \
+        set_default, read, release) \
+    if (arguments.name.initialized) { \
+        name##_t *target = &arguments.name.value; \
+        \
+        do { \
+            release \
+        } while (0); \
+        \
+        if (target); \
+    }
+#include "arguments.def"
+}
+
+
+/**
+ * Parses the command line given by argv and argc.
+ *
+ * If ARGUMENTS_AUTOMATIC is not defined, this function will simply parse the
+ * arguments from argv[*nextarg] until either the end of argv is reached or an
+ * invalid argument value is encountered.
+ *
+ * If ARGUMENTS_PRINT_HELP is defined, this function will print a help message
+ * if the arguments --help or -h are encountered. If ARGUMENTS_AUTOMATIC is also
+ * defined, this function will not return; rather, it will terminate the process
+ * with the return code 0.
+ *
+ * If ARGUMENTS_AUTOMATIC is defined, this function will automatically
+ * initialise the argument struct and then parse all arguments. If an invalid
+ * argument value is encountered, it will not return; rather, it will terminate
+ * the process with the return code 1.
+ *
+ * @param argv
+ *     The argument vector, typically the same as argv of the standard main
+ *     function.
+ * @param argc
+ *     The number of elements in argv.
+ * @param nextarg
+ *     The index of the first argument to examine. Upon return, the int pointed
+ *     to by this argument will contain the index of the next unparsed argument.
+ *     If nextarg == argc, all arguments were parsed. If an error occurred
+ *     while parsing an argument, nextarg will contain its index.
+ * @return AC_OK if no error occurred, AC_HELP if --help was encountered and
+ *     ARGUMENTS_PRINT_HELP was defined, or AC_INVALID if an invalid value was
+ *     passed
+ */
+static int
+arguments_read(int argc, char *argv[], int *nextarg)
+{
+    int is_valid = 1;
+
+    if (*nextarg < 1) {
+        *nextarg = 1;
+    }
+
+    /* Parse the command line */
+    while (*nextarg < argc && is_valid) {
+        /* If ARGUMENTS_PRINT_HELP is defined, we handle --help and -h */
+#ifdef ARGUMENTS_PRINT_HELP
+        if ((strcmp(argv[*nextarg], "--help") == 0)
+                || (strcmp(argv[*nextarg], "-h") == 0)) {
+            arguments_print_help();
+            return AC_HELP;
+        }
+#else
+        /* If ARGUMENTS_PRINT_HELP is not defined, we need a dummy if statement
+           to allow the following list of else ifs */
+        if (0);
+#endif
+
+    #undef _A
+    #define _A(type, name, short, value_count, is_required, help, \
+            set_default, read, release) \
+        else if ((arguments_cmp(argv[*nextarg], #name) == 0) \
+                || (short && strcmp(argv[*nextarg], short) == 0)) { \
+            arguments.name.present = 1; \
+            arguments.name.value_strings_length = value_count; \
+            \
+            if (*nextarg + arguments.name.value_strings_length < argc) { \
+                (*nextarg)++; \
+                arguments.name.value_strings = argv + *nextarg; \
+                *nextarg += arguments.name.value_strings_length; \
+            } \
+            else { \
+                is_valid = 0; \
+                break; \
+            } \
+        }
+    #include "arguments.def"
+
+        else {
+            /* If no argument matched the current one, break */
+            break;
+        }
+    }
+
+    /* is_valid it non-zero unless a command line parameter is missing */
+    return is_valid ? AC_OK : AC_ERROR;
+}
+
+/**
+ * Call this function after all invocations of arguments_read.
+ *
+ * It will set the default values for all arguments that have not been passed on
+ * the command line. If an argument that was required has not been passed, this
+ * function will return AC_ERROR.
+ *
+ * @return AC_OK if all required arguments have been passed, or AC_ERROR if a
+ *     parameter error is encountered
+ */
+static int
+arguments_set(void)
+{
+    int is_valid = 1;
+
+#undef _A
+#define _A(type, name, short, value_count, is_required, help, \
+        set_default, read, release) \
+    if (is_valid) { \
+        name##_t *target = &arguments.name.value; \
+        char **value_strings = \
+            arguments.name.value_strings; \
+        unsigned int value_strings_length = \
+            arguments.name.value_strings_length; \
+        \
+        if (arguments.name.present) { \
+            read \
+        } \
+        else { \
+            set_default \
+        } \
+        \
+        if (is_valid) { \
+            arguments.name.initialized = 1; \
+        } \
+        \
+        if (target); \
+        if (value_strings); \
+        if (value_strings_length); \
+    }
+#include "arguments.def"
+
+    /* This function has to be called */
+    atexit(arguments_release);
+
+    return is_valid ? AC_OK : AC_ERROR;
+}
+
+
+/*
+ * If ARGUMENTS_AUTOMATIC is defined, we implement main() and call start()
+ * instead.
+ */
+#ifdef ARGUMENTS_AUTOMATIC
+
+int
+main(int argc, char *argv[])
+{
+    int result;
+    int nextarg = 1;
+
+    arguments_initialize();
+
+    /* First parse the arguments */
+    switch (arguments_read(argc, argv, &nextarg)) {
+    case AC_OK:
+        /* Just continue if no invalid arguments were found */
+        break;
+
+    case AC_HELP:
+        return 0;
+
+    case AC_ERROR:
+        /* A parameter requiring values was not passed enough values before the
+           command line ended */
+        return ARGUMENTS_PARAMETER_INVALID;
+    }
+
+    /* Detect missing arguments */
+#ifdef ARGUMENTS_PRINT_MISSING
+    #define print_missing(name) \
+        fprintf(stderr, "Argument %s is required, but not specified.\n", name)
+#else
+    #define print_missing(name)
+#endif
+
+#undef _A
+#define _A(type, name, short, value_count, is_required, help, \
+        set_default, read, release) \
+    if ((is_required) && !arguments.name.present) { \
+        print_missing(#name); \
+        return ARGUMENTS_PARAMETER_MISSING; \
+    }
+#include "arguments.def"
+
+    /* Call setup before converting the parameter values to variables */
+    result = setup(argc, argv);
+    if (result != 0) {
+        return result;
+    }
+
+    /* Once we have successfully called setup, we must make sure that teardown
+       is also called */
+    atexit(teardown);
+
+    /* Actually converts the string values read to variables */
+    switch (arguments_set()) {
+    case AC_OK:
+        /* Just continue if no invalid arguments were found */
+        break;
+
+    default:
+        /* A parameter requiring values was not passed enough values before the
+           command line ended */
+        return ARGUMENTS_PARAMETER_INVALID;
+    }
+
+    return run(argc, argv
+        #undef _A
+        #define _A(type, name, short, value_count, is_required, help, \
+                set_default, read, release) \
+            , arguments.name.value
+        #include "arguments.def"
+    );
+}
+
+
+#define main run
+
+#endif
